73229103baef94d28eddc4c65d202f9aa9c1a678
Katherine McClintic <kdmcclin@gmail.com>
2015-04-07 10:40:25 -0400

    Update associations in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :report
+	
 	validates :content, presence: :true
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,7 @@
 class Report < ActiveRecord::Base
+	belongs_to :user
+	has_many :comments
+
 	validates	:generation,	presence: true
 	validates	:content, 		presence: true
 end
