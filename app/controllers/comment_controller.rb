d33857636c7f9f7e3eff7999dd259e9bd405a791
Katherine McClintic <kdmcclin@gmail.com>
2015-04-07 11:49:59 -0400

    Start adding functionality for comments

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+	before_action :get_report
+	before_action :authenticate_user!
+
+	# def new
+	# 	@comment = Comment.new
+	# end
+
+	def create
+		@comment = @report.comments.create(comment_params)
+		@comment.user_id = current_user.id
+		if comment.save
+			redirect_to report_path(@report)
+		else
+			flash[:error] = "Comment could not be saved."
+			render :action => "new"
+		end
+
+		#TODO - AJAX this later
+	end
+
+	def destroy
+		@comment = @report.comments.find(params[:id])
+		redirect_to report_path(@report)
+
+		#TODO - AJAX this later
+	end
+
+	private
+
+	def get_report
+		@report = Report.find(params[:report_id])
+	end
+
+	def comment_params
+		params.require(:comment).permit([:content])
+	end
+end
