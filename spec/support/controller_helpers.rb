5f196758595ce7627a568d559d67e85b3484dc72
Katherine McClintic <kdmcclin@gmail.com>
2015-04-14 22:44:33 -0400

    Simplify test user login helper

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,13 +1,8 @@
 module ControllerHelpers
-	def sign_in(user = double("User"))
-  	if user.nil?
-  		allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
-  		allow(controller).to receive(:current_user).and_return(nil)
-  	else
-  		allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-  		allow(controller).to receive(:current_user).and_return(user)
-  	end
-  end
+    def login_user
+        user = FactoryGirl.create(:user)
+        sign_in user
+    end
 end
 
 RSpec.configure do |config|
