ba2c7ba12cfd5a276bc80fe0d6a7c0f5881d860d
Katherine McClintic <kdmcclin@gmail.com>
2015-05-09 15:31:16 -0400

    Add necessary page method to custom state route creation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -43,10 +43,10 @@ class ReportsController < ApplicationController
 		state = array.first
 		define_method(state.downcase) do
 			if params[:search]
-				@reports = Report.search_text(params[:search]) params[:page]
+				@reports = Report.search_text(params[:search]).page params[:page]
 				@search_term = params[:search]
 			else
-				@reports = Report.where(:state => "#{state}").order(updated_at: :desc) params[:page]
+				@reports = Report.where(:state => "#{state}").order(updated_at: :desc).page params[:page]
 			end
 		end
 	end
