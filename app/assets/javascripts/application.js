a77ea7794560d1d3a188bc5165d7bf430b76d908
Katherine McClintic <kdmcclin@gmail.com>
2015-06-29 18:18:24 -0400

    Add js for sticky footer effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,15 @@
 //= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation(); 
+	var footer = $("#footer");
+  var pos = footer.position();
+  var height = $(window).height();
+  height = height - pos.top;
+  height = height - footer.height();
+  if (height > 0) {
+      footer.css({
+          'margin-top': height + 'px'
+      });
+  }
+});
