468a93eda36ea1b50f64fabd51adcdc657d954c3
Katherine McClintic <kdmcclin@gmail.com>
2015-09-03 16:31:38 -0400

    Add comment post tests

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe CommentsController do
+	
+	context 'POST #create' do
+		it "blocks unauthenticated access" do
+			report = FactoryGirl.create :report
+			post :create, report_id: report.id
+			expect(response).to redirect_to(new_user_session_path)
+		end
+
+		it "allows logged in users to have access" do
+			login_user
+			report = FactoryGirl.create :report
+			post :create, report_id: report.id, comment: FactoryGirl.attributes_for(:comment)
+			expect(response).to_not redirect_to(new_user_session_path)
+		end
+
+		context "with valid attributes" do
+			it "saves the new comment in the database" do
+				report = FactoryGirl.create :report
+				expect{FactoryGirl.create :comment, report_id: report.id}.to change(Comment, :count).by(1)
+			end
+
+			it "redirects to the comment's report page" do
+				login_user
+				report = FactoryGirl.create :report
+				post :create, report_id: report.id, comment: FactoryGirl.attributes_for(:comment)
+				expect(response).to redirect_to report_path(report)
+			end
+		end
+
+		context "with invalid attributes"do
+			it "does not save the new comment in the database" do
+				report = FactoryGirl.create :report
+				expect{FactoryGirl.build :invalid_comment, report_id: report.id}.to_not change(Comment, :count)
+			end
+
+			it "re-renders the new comment template" do
+				login_user
+				report = FactoryGirl.create :report
+				post :create, report_id: report.id, comment: FactoryGirl.attributes_for(:invalid_comment)
+				expect(response).to redirect_to report_path(report)
+			end
+		end
+	end
+end
